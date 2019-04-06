15d77c234ac2149bf6f84ef0c3b8ab738a90216d
Dr. Rich Cordero <rich@corderophilosophy.com>
2019-04-05 22:04:01 -0400

    Begin adding "commit" functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use std::env::current_dir;
 use std::fs;
 use std::fs::DirBuilder;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process;
 
 #[derive(Debug)]
 pub struct Config {
-    pub command: String,
-    pub path: String,
+    command: String,
+    path: String,
 }
 
 impl Config {
@@ -28,6 +28,48 @@ impl Config {
         };
 
         Ok(Config { command, path })
+    }
+}
+
+#[derive(Debug)]
+pub struct Repo {
+    root_path: Option<PathBuf>,
+    git_path: Option<PathBuf>,
+    db_path: Option<PathBuf>,
+    file_list: Option<Vec<PathBuf>>,
+}
+impl Repo {
+    pub fn new(
+        root_path: Option<PathBuf>,
+        git_path: Option<PathBuf>,
+        db_path: Option<PathBuf>,
+    ) -> Repo {
+        let mut repo = Repo {
+            root_path,
+            git_path,
+            db_path,
+            file_list: None,
+        };
+        repo.read_files();
+        repo
+    }
+    pub fn read_files(&mut self) {
+        let local_dirs = fs::read_dir(".").unwrap();
+
+        let mut dirs = vec![];
+
+        for entry in local_dirs {
+            let entry = entry.unwrap();
+            let path = fs::canonicalize(entry.path()).unwrap();
+
+            if path == fs::canonicalize(PathBuf::from(".git")).unwrap() {
+                continue;
+            } else {
+                dirs.push(path);
+            }
+        }
+
+        self.file_list = Some(dirs);
     }
 }
 
@@ -53,6 +95,12 @@ pub fn run(config: Config) -> Result<(), &'static str> {
                 "Initialized empty grist repository in {}",
                 fs::canonicalize(git_path).unwrap().display()
             );
+        }
+        "commit" => {
+            let root_path = current_dir().unwrap();
+            let git_path = root_path.join(".git");
+            let db_path = git_path.join("objects");
+            Repo::new(Some(root_path), Some(git_path), Some(db_path));
         }
         _ => eprintln!("grist: {} is not a grist command", command),
     }
